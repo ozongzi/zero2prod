0c2a6fa8968f10a67571e470aab9b96f21268383
ozongzi <ozongzi@icloud.com>
2026-02-08 14:17:54 +0800

    added crate reqwest as dev dependencies to test /health_check added test to test /health_check changed app logic to let "run" returns a server and let main.rs run it.

---
+++ tests/health_check.rs
@@ -0,0 +1,23 @@
+///
+/// # Health Check
+/// when GET /health_check, it should returns code 200 with no body
+
+#[tokio::test]
+async fn health_check_works() {
+    spawn_app();
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute the request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+}

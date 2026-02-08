88c662c13677771d252ef959e8476668823d199d
ozongzi <ozongzi@icloud.com>
2026-02-08 22:21:58 +0800

    update subscibe function to get it really work and changed run and main to get connetion, test remain to be fixed

--- src/startup.rs
+++ src/startup.rs
@@ -1,12 +1,15 @@
 use crate::routes::*;
 use actix_web::{App, HttpServer, dev::Server, web};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> std::io::Result<Server> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> std::io::Result<Server> {
+    let db_pool = web::Data::new(db_pool);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();

ae10fff6530d5f9ecaf18697fea5afa078a971f7
ozongzi <ozongzi@icloud.com>
2026-02-09 09:47:49 +0800

    changed toml to add log crate and change version anotations to enable project to use newer verisons of crates. record the subscriptions

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -24,9 +24,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("New subscriber details have been saved");
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }

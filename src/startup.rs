ea69ce6a3f746b385d886057b19318af07626f6d
ozongzi <ozongzi@icloud.com>
2026-02-09 12:47:38 +0800

    use tracing_actix_web::TracingLogger

--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,14 @@
 use crate::routes::*;
-use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer, dev::Server, web};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> std::io::Result<Server> {
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())

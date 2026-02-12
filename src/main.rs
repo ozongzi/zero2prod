07d1b2b81e12ea6d4a03feebb663cd61fd3998dc
ozongzi <ozongzi@icloud.com>
2026-02-12 23:04:43 +0800

    tt

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,7 @@ async fn main() -> std::io::Result<()> {
     let connection_pool =
         PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres.");
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
+    let address = format!("{}:{}", "0.0.0.0", configuration.application.port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
 }
